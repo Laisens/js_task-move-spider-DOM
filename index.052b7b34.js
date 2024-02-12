ce171c8c80e9a74afe99f4608a1fab17f057c5fe
Siarhei Tsishkavets <kenny.sergey@gmail.com>
2024-02-12 20:01:36 +0100

    make build

---
+++ index.052b7b34.js
@@ -0,0 +1,3 @@
+const t=document.querySelector(".wall"),e=document.querySelector(".spider");document.addEventListener("click",l=>{if(l.target===t){let i=t.clientHeight-e.clientHeight,c=t.clientWidth-e.clientWidth,n=l.offsetX-e.clientHeight/2,o=l.offsetY-e.clientWidth/2;n>i?e.style.left=i+"px":n<0?e.style.left="0px":e.style.left=n+"px",o>c?e.style.top=c+"px":o<0?e.style.top="0px":e.style.top=o+"px"}});//# sourceMappingURL=index.052b7b34.js.map
+
+//# sourceMappingURL=index.052b7b34.js.map
